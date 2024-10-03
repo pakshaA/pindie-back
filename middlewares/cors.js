5caf2785e4e5d9c0968e040c970d187a04c16c20
Artem <pakshaartem@gmail.com>
2024-10-03 19:47:25 +0300

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
   'https://students-projects.ru',
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://pindiebackendpaksh.nomoredomainswork.ru/'
+  'https://pindie-back.vercel.app/'
 ];
 
 function cors(req, res, next) {
